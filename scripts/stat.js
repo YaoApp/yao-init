ec933e5f992e11e9a97730553a8d1a0545affa93
QJAutumn <780588304@qq.com>
2023-05-15 15:29:36 +0800

    [add]pet新增组件

--- scripts/stat.js
+++ scripts/stat.js
@@ -36,3 +36,29 @@ function Income(field, value, data) {
   );
   return value;
 }
+
+function OnChange(query) {
+  // 进入onchange事件
+  query = query || {};
+  field = query.key;
+  newVal = query.value;
+  oldVal = query.old;
+
+  let data = { query: query };
+  if (newVal == "cat") {
+    data.cost = 1000;
+  } else {
+    data.cost = 2000;
+  }
+
+  let setting = Process("yao.form.Setting", "demo.pet");
+
+  if (setting && setting.code && setting.message) {
+    throw new Exception(setting.message, 500);
+  }
+
+  return {
+    data: data,
+    setting: setting,
+  };
+}
